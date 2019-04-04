74ace24472a3b5a21f7d820bc6b8b7987dc752be
gaomengen <gaomengen@yeah.net>
2019-04-04 08:40:56 -0400

    Update bq_cost_analysis.sql

--- bq_cost_analysis.sql
+++ bq_cost_analysis.sql
@@ -1,4 +1,4 @@
-SELECT service.description, cost 
-  FROM [free-trial-billing:billing_export.table_name] 
+SELECT service.description, service.id, cost,usage_start_time, usage_end_time, billing_account_id
+  FROM `free-trial-billing.billing_export.gcp_billing_export_v1_01E957_57E2EE_7ED151`
   WHERE cost > 0
-  ORDER BY cost DESC
+  ORDER BY usage_end_time DESC
